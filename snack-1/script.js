af2c31211a2c096e1389153b110616cbadb6a348
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-29 11:12:20 +0200

    bonus

--- snack-1/script.js
+++ snack-1/script.js
@@ -4,13 +4,13 @@ const getPostTitle = (id) => {
   return new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
       .then((response) => response.json())
-      .then((obj) => resolve(obj))
+      .then((post) => resolve(post))
       .catch(reject);
   });
 };
 
 getPostTitle(1)
-  .then((obj) => console.log("titolo del post =", obj.title))
+  .then((post) => console.log("titolo del post =", post.title))
   .catch((error) => console.error(error));
 
 // RECUPERO I DATI DELL'AUTORE
@@ -19,11 +19,29 @@ const getPost = (id) => {
   return new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
       .then((response) => response.json())
-      .then((obj) => resolve(obj))
+      .then((post) => {
+        resolve(post);
+      })
+      .catch(reject);
+  });
+};
+
+const autorGet = (userId) => {
+  return new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/users/${userId}`)
+      .then((response) => response.json())
+      .then((autor) => resolve(autor))
       .catch(reject);
   });
 };
 
 getPost(1)
-  .then((obj) => console.log(obj))
+  .then((post) => {
+    post;
+    return autorGet(post.userId).then((autor) => {
+      autor;
+
+      console.log("Post:", post, "Autore:", autor);
+    });
+  })
   .catch((error) => console.error(error));
